04436697b726bbc22b264fd2ae802d5819012550
ivan <ivangonzaleztorre@gmail.com>
2022-07-04 09:34:01 +0200

    init

--- finetune_iberspeech1.sh
+++
@@ -1,6 +0,0 @@
-nvidia-docker run -it --rm --network=host --name "fairseq_container" --shm-size=4g --ulimit memlock=-1 \
--v /home/:/home/ -v /data/:/data/ -w $PWD fairseq fairseq-hydra-train --config-dir config \
---config-name finetune_iberspeech1.yaml
-
-
-
---
+++ finetune_quechua.sh
@@ -0,0 +1,6 @@
+nvidia-docker run -it --rm --network=host --name "fairseq_quechua" --shm-size=4g --ulimit memlock=-1 \
+-v /home/:/home/ -v /data/:/data/ -w $PWD fairseq fairseq-hydra-train --config-dir config \
+--config-name finetune_qeuchua.yaml
+
+
+
