cf2590df3e4850d079e6aac1137a184fb0053b54
ivan <ivangonzaleztorre@gmail.com>
2022-07-04 09:35:56 +0200

    init

--- finetune_quechua.sh
+++ finetune_quechua.sh
@@ -1,6 +1,9 @@
-nvidia-docker run -it --rm --network=host --name "fairseq_quechua" --shm-size=4g --ulimit memlock=-1 \
+docker run -it --rm --network=host --name "fairseq_quechua" --shm-size=4g --ulimit memlock=-1 \
 -v /home/:/home/ -v /data/:/data/ -w $PWD fairseq fairseq-hydra-train --config-dir config \
 --config-name finetune_qeuchua.yaml
+#nvidia-docker run -it --rm --network=host --name "fairseq_quechua" --shm-size=4g --ulimit memlock=-1 \
+#-v /home/:/home/ -v /data/:/data/ -w $PWD fairseq fairseq-hydra-train --config-dir config \
+#--config-name finetune_qeuchua.yaml
 
 
 
